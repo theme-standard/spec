92c7bb9aad7231936f846468be5db4eacea7ddf5
Cao, Shawn <SCao@cvent.com>
2017-03-07 19:14:10 -0500

    flexible useTheme utility to handle components migration

--- react-examples/themed-button/app.js
+++ react-examples/themed-button/app.js
@@ -12,12 +12,18 @@ const theme = {
 
 // ThemedButton component has now Theme built in, we don't need to pass it over
 // every time we use it.
-const ThemedButton = useTheme(Button, theme)
+const ThemedButton = useTheme(Button, { classes: theme.classes })
 
 export default function App() {
   return (
     <div>
-      <ThemedButton />
+      <ThemedButton icon="star" />
     </div>
   )
+  // compared with using plain components where styling props have to be manually passed in
+  // (
+  //   <div>
+  //     <Button classes={{ button: theme.classes.button, icon: theme.classes.icon }} icon="star" />
+  //   </div>
+  // )
 }
--- react-examples/themed-button/useTheme.js
+++ react-examples/themed-button/useTheme.js
@@ -3,10 +3,23 @@ import React, {PropTypes} from 'react'
 /**
  * Wrapps the passed component with a Theme component, which will always pass
  * the theme as a prop.
+ * NOTE: useTheme has challenge predicting what the original component expects
+ * @param {Object} Component - React component being wrapped
+ * @param {Object} themeMapping - mapping of theme to components props
  */
-export default function useTheme(Component, theme) {
+export default function useTheme(Component, themeMapping) {
   function Theme(props) {
-    return <Component {...props} theme={theme} />
+    let theme, rest;
+    // take theme related props out and map, then pass rest of them down
+    Object.keys(props).forEach((key, value) => {
+      if(themeMapping.hasOwnProperty(key)) {
+        theme.key = themMapping[key]
+      } else {
+        rest.key = value
+      }
+    })
+    // TODO: test this utility function and also add test/UI for all react-examples
+    return <Component {...theme} {...rest} />
   }
 
   Theme.propTypes = {
